7388590e35948dfb989d2e6e5645aefd3cdd51c4
Najib <44539538+N4J13@users.noreply.github.com>
2024-01-16 15:56:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import { AppRoutes } from "./routes";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <div >
-      <AppRoutes  />
+    <div className="font-main">
+      <AppRoutes />
       <Toaster />
     </div>
   );
